e2f065cdd4f4f021f11fcbcd9091cf3112bf6991
JMax45 <maxmailru.45@gmail.com>
2022-09-10 07:56:52 +0000

    test: add CacheContainer tests

---
+++ tests/CacheContainer.test.ts
@@ -0,0 +1,34 @@
+import { constants } from '../src/constants';
+import makeid from '../src/utils/makeid';
+import Byteroo from '../src';
+
+const wait = (n: number) => new Promise((resolve) => setTimeout(resolve, n));
+
+describe('test CacheContainer', () => {
+  it('test set and get method', async () => {
+    const storage = new Byteroo({
+      name: makeid(8),
+      path: constants.IN_MEMORY_STORAGE,
+    });
+    const container = await storage.getContainer(makeid(8), {
+      type: 'cache',
+      ttl: 1,
+    });
+    container.set('testval', 'testval');
+    expect(container.get('testval')).not.toBeUndefined();
+    await wait(2000);
+    expect(container.get('testval')).toBeUndefined();
+  });
+  it('test list method', async () => {
+    const storage = new Byteroo({
+      name: makeid(8),
+      path: constants.IN_MEMORY_STORAGE,
+    });
+    const container = await storage.getContainer(makeid(8), {
+      type: 'cache',
+      ttl: 1,
+    });
+    container.set('testval', 'testval');
+    expect(container.list()[0]).toBe('testval');
+  });
+});
