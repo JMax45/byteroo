360d8c38cb30dbeda01a64bd281c8e9b87211e5f
JMax45 <maxmailru.45@gmail.com>
2023-01-06 08:47:58 +0000

    test: CacheContainer's set ttl parameter

--- tests/CacheContainer.test.ts
+++ tests/CacheContainer.test.ts
@@ -31,4 +31,20 @@ describe('test CacheContainer', () => {
     container.set('testval', 'testval');
     expect(container.list()[0]).toBe('testval');
   });
+  it('test TTL parameter in set method', async () => {
+    const storage = new Byteroo({
+      name: makeid(8),
+      path: constants.IN_MEMORY_STORAGE,
+    });
+    const container = await storage.getContainer(makeid(8), {
+      type: 'cache',
+      ttl: 1,
+    });
+    container.set('testval', 'testval', 3);
+    expect(container.get('testval')).not.toBeUndefined();
+    await wait(2000);
+    expect(container.get('testval')).not.toBeUndefined();
+    await wait(1500);
+    expect(container.get('testval')).toBeUndefined();
+  });
 });
